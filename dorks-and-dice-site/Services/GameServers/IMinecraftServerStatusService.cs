2c2300a968d446359c5b3b22b7979cc224d6bae9
Kyle Barnett <kbarnett0306@gmail.com>
2026-08-05 06:49:21 -0400

    Remove superseded flat Minecraft status interface

--- dorks-and-dice-site/Services/GameServers/IMinecraftServerStatusService.cs
+++
@@ -1,8 +0,0 @@
-using dorks_and_dice_site.Models.GameServers;
-
-namespace dorks_and_dice_site.Services.GameServers;
-
-public interface IMinecraftServerStatusService
-{
-    Task<MinecraftServerStatus> GetStatusAsync(CancellationToken cancellationToken = default);
-}
