33a5daa223b03f3d1ee2adc62ca4acb19d482e03
Kyle Barnett <kbarnett0306@gmail.com>
2026-08-05 10:47:44 -0400

    Remove temporary Hytale status interface

--- dorks-and-dice-site/Services/GameServers/Hytale/IHytaleServerStatusService.cs
+++
@@ -1,8 +0,0 @@
-using dorks_and_dice_site.Models.GameServers.Hytale;
-
-namespace dorks_and_dice_site.Services.GameServers.Hytale;
-
-public interface IHytaleServerStatusService
-{
-    Task<HytaleServerStatus> GetStatusAsync(CancellationToken cancellationToken = default);
-}
