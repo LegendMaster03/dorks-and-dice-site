4b20bead1b7226a7f08d4c68074dce58f8e20bce
Kyle Barnett <kbarnett0306@gmail.com>
2026-09-02 02:12:31 -0400

    Represent global privileged roles on account settings

--- dorks-and-dice-site/Models/Identity/AccountViewModel.cs
+++ dorks-and-dice-site/Models/Identity/AccountViewModel.cs
@@ -7,7 +7,9 @@ public sealed class AccountViewModel
     public Guid UserId { get; init; }
     public string Email { get; init; } = string.Empty;
     public bool IsAdministrator { get; init; }
-    public bool CanClaimAdministrator { get; init; }
+    public bool IsDeveloper { get; init; }
+    public bool HasTrustedAccess { get; init; }
+    public bool CanBootstrapPrivilegedAccess { get; init; }
 
     [Required]
     [StringLength(ApplicationUser.DisplayNameMaxLength, MinimumLength = 1)]
