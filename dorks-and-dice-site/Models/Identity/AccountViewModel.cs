3ccf78d8dcd4163770b735aebffa099a64e0ebb7
Kyle Barnett <kbarnett0306@gmail.com>
2026-09-02 01:33:50 -0400

    Expose administrator state in account settings

--- dorks-and-dice-site/Models/Identity/AccountViewModel.cs
+++ dorks-and-dice-site/Models/Identity/AccountViewModel.cs
@@ -6,6 +6,8 @@ public sealed class AccountViewModel
 {
     public Guid UserId { get; init; }
     public string Email { get; init; } = string.Empty;
+    public bool IsAdministrator { get; init; }
+    public bool CanClaimAdministrator { get; init; }
 
     [Required]
     [StringLength(ApplicationUser.DisplayNameMaxLength, MinimumLength = 1)]
