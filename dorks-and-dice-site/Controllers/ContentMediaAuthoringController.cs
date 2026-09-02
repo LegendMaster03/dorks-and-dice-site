89ea0660ac02858fd6a7dfe90e9e78789976dee1
Kyle Barnett <kbarnett0306@gmail.com>
2026-09-02 02:11:25 -0400

    Require developer role for media authoring

--- dorks-and-dice-site/Controllers/ContentMediaAuthoringController.cs
+++ dorks-and-dice-site/Controllers/ContentMediaAuthoringController.cs
@@ -1,11 +1,14 @@
 using dorks_and_dice_site.Models.Content;
 using dorks_and_dice_site.Services.Content;
 using dorks_and_dice_site.Services.Content.Storage;
+using dorks_and_dice_site.Services.Identity;
 using dorks_and_dice_site.Services.Site;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace dorks_and_dice_site.Controllers;
 
+[Authorize(Policy = AuthorizationPolicies.DevAccess)]
 [Route("development/content/{slug}/media")]
 [RequestSizeLimit(ContentInputPolicy.MaxAssetUploadBytes + 65_536)]
 public sealed class ContentMediaAuthoringController : Controller
