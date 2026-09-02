4d81e42ce95f5d66008e36ad3e7765ae106a0aa2
Kyle Barnett <kbarnett0306@gmail.com>
2026-09-02 02:09:38 -0400

    Add global developer role

--- dorks-and-dice-site/Services/Identity/AccountRoles.cs
+++ dorks-and-dice-site/Services/Identity/AccountRoles.cs
@@ -3,4 +3,7 @@ namespace dorks_and_dice_site.Services.Identity;
 public static class AccountRoles
 {
     public const string Admin = "Admin";
+    public const string Dev = "Dev";
+
+    public static IReadOnlyList<string> Privileged { get; } = [Admin, Dev];
 }
