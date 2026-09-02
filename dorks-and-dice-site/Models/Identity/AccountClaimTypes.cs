68c282189d7d47d8aff9ae47c5ca1e7d808727bd
Kyle Barnett <kbarnett0306@gmail.com>
2026-09-02 02:09:10 -0400

    Add scoped role claim type

--- dorks-and-dice-site/Models/Identity/AccountClaimTypes.cs
+++ dorks-and-dice-site/Models/Identity/AccountClaimTypes.cs
@@ -3,4 +3,5 @@ namespace dorks_and_dice_site.Models.Identity;
 public static class AccountClaimTypes
 {
     public const string DisplayName = "display_name";
+    public const string ScopedRole = "scoped_role";
 }
