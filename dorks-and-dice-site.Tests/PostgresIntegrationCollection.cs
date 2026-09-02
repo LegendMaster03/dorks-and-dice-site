4a46f24c567c2c5b19ed931bdaafd5a74ea4669c
Kyle Barnett <kbarnett0306@gmail.com>
2026-09-02 18:23:29 -0400

    Serialize shared PostgreSQL integration tests

---
+++ dorks-and-dice-site.Tests/PostgresIntegrationCollection.cs
@@ -0,0 +1,7 @@
+namespace dorks_and_dice_site.Tests;
+
+[CollectionDefinition(Name, DisableParallelization = true)]
+public sealed class PostgresIntegrationCollection
+{
+    public const string Name = "PostgreSQL integration";
+}
