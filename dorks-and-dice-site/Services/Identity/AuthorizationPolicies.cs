93b1c40906b70c98ecb130d3651b1783039bdd96
Kyle Barnett <kbarnett0306@gmail.com>
2026-09-02 02:08:45 -0400

    Add trusted privileged authorization policies

---
+++ dorks-and-dice-site/Services/Identity/AuthorizationPolicies.cs
@@ -0,0 +1,10 @@
+namespace dorks_and_dice_site.Services.Identity;
+
+public static class AuthorizationPolicies
+{
+    public const string TrustedAccess = "TrustedAccess";
+    public const string AdminAccess = "AdminAccess";
+    public const string DevAccess = "DevAccess";
+    public const string PrivilegedAccess = "PrivilegedAccess";
+    public const string AdminAndDevAccess = "AdminAndDevAccess";
+}
