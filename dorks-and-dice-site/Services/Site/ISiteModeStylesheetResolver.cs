6037ca64af1fb583a32d3b92d2d50e3248cf4240
Kyle Barnett <kbarnett0306@gmail.com>
2026-09-05 08:59:23 -0400

    Add request-context stylesheet resolution contract

--- dorks-and-dice-site/Services/Site/ISiteModeStylesheetResolver.cs
+++ dorks-and-dice-site/Services/Site/ISiteModeStylesheetResolver.cs
@@ -4,5 +4,8 @@ namespace dorks_and_dice_site.Services.Site;
 
 public interface ISiteModeStylesheetResolver
 {
+    IReadOnlyList<string> GetStylesheetPaths(SiteModeContext context);
+
+    // Compatibility overload for callers that have not yet migrated off the legacy enum.
     IReadOnlyList<string> GetStylesheetPaths(SiteMode siteMode, bool includeDevelopmentTools);
 }
