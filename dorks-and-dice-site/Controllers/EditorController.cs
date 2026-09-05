78cf0491e60bd8550143c25b97edd25e33618e74
Kyle Barnett <kbarnett0306@gmail.com>
2026-09-05 10:19:10 -0400

    Build editor landing from registered modes

--- dorks-and-dice-site/Controllers/EditorController.cs
+++ dorks-and-dice-site/Controllers/EditorController.cs
@@ -1,5 +1,7 @@
+using dorks_and_dice_site.Models.Editor;
 using dorks_and_dice_site.Models.Identity;
 using dorks_and_dice_site.Services.Identity;
+using dorks_and_dice_site.Services.Site;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -9,26 +11,61 @@ namespace dorks_and_dice_site.Controllers;
 [Route("editor")]
 public sealed class EditorController : Controller
 {
+    private readonly ISiteModeRegistry _siteModeRegistry;
+    private readonly SiteModeOptions _siteModeOptions;
+
+    public EditorController(
+        ISiteModeRegistry siteModeRegistry,
+        SiteModeOptions siteModeOptions)
+    {
+        _siteModeRegistry = siteModeRegistry;
+        _siteModeOptions = siteModeOptions;
+    }
+
     [HttpGet("")]
     public IActionResult Index()
     {
-        var isAdmin = User.IsInRole(AccountRoles.Admin);
-        var canEditDorksAndDice = isAdmin || HasScopedEditorRole(AccountRoleScopes.DorksAndDice);
-        var canEditProfessional = isAdmin || HasScopedEditorRole(AccountRoleScopes.Professional);
+        var modeContext = HttpContext.GetSiteModeContext();
+        var hasGlobalEditor = AccountRoleHierarchy.PrincipalHasGlobalRole(User, AccountRoles.GlobalEditor);
+        var modes = _siteModeRegistry.All
+            .Where(mode => hasGlobalEditor || HasScopedEditorRole(mode.Id))
+            .Select(mode => new EditorModeOption
+            {
+                ModeId = mode.Id,
+                DisplayName = mode.DisplayName,
+                EditorHref = modeContext.HasTrustedAccess
+                    ? null
+                    : ResolvePublicEditorHref(modeContext.ActiveModeId, mode.Id)
+            })
+            .ToList();
 
-        if (!canEditDorksAndDice && !canEditProfessional)
+        if (modes.Count == 0)
         {
             return Forbid();
         }
 
-        ViewData["CanEditDorksAndDice"] = canEditDorksAndDice;
-        ViewData["CanEditProfessional"] = canEditProfessional;
-        return View();
+        return View(new EditorIndexViewModel
+        {
+            IsTrustedPreview = modeContext.HasTrustedAccess,
+            Modes = modes
+        });
     }
 
     private bool HasScopedEditorRole(string scope)
     {
         var expectedValue = $"{scope}:{ScopedAccountRoles.Editor}";
         return User.HasClaim(AccountClaimTypes.ScopedRole, expectedValue);
+    }
+
+    private string? ResolvePublicEditorHref(string? activeModeId, string targetModeId)
+    {
+        if (string.Equals(activeModeId, targetModeId, StringComparison.OrdinalIgnoreCase))
+        {
+            return "/editor/content";
+        }
+
+        return _siteModeOptions.TryGetCanonicalHost(targetModeId, out var canonicalHost)
+            ? $"https://{canonicalHost}/editor/content"
+            : null;
     }
 }
