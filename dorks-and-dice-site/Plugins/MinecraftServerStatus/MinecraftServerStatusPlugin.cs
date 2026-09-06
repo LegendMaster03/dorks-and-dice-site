cfec7cd529b0aa5d524d0b9980c3dbebef9d6436
Kyle Barnett <kbarnett0306@gmail.com>
2026-09-06 03:13:57 -0400

    Split Minecraft status into composable page components

--- dorks-and-dice-site/Plugins/MinecraftServerStatus/MinecraftServerStatusPlugin.cs
+++ dorks-and-dice-site/Plugins/MinecraftServerStatus/MinecraftServerStatusPlugin.cs
@@ -11,20 +11,54 @@ public sealed class MinecraftServerStatusPlugin : ISitePlugin
     public SitePluginManifest Manifest { get; } = new(
         Id: "minecraft-server-status",
         DisplayName: "Minecraft Server Status",
-        Version: "1.0.0");
+        Version: "1.1.0");
 
     public void RegisterServices(IServiceCollection services)
     {
         services.AddOptions<MinecraftServerOptions>()
             .BindConfiguration(MinecraftServerOptions.SectionName);
         services.AddSingleton<IMinecraftServerStatusService, MinecraftServerStatusService>();
-        services.AddSingleton<IContentPageComponentDefinition, MinecraftServerStatusPageComponentDefinition>();
+        services.AddSingleton<MinecraftServerStatusSnapshotStore>();
+        services.AddSingleton<IMinecraftServerStatusSnapshotStore>(serviceProvider =>
+            serviceProvider.GetRequiredService<MinecraftServerStatusSnapshotStore>());
+        services.AddHostedService<MinecraftServerStatusPoller>();
+
+        services.AddSingleton<IContentPageComponentDefinition>(
+            new MinecraftServerStatusPageComponentDefinition(
+                "minecraft-server-status",
+                "MinecraftServerStatus"));
+        services.AddSingleton<IContentPageComponentDefinition>(
+            new MinecraftServerStatusPageComponentDefinition(
+                "minecraft-server-status-badge",
+                "MinecraftServerStatusField"));
+        services.AddSingleton<IContentPageComponentDefinition>(
+            new MinecraftServerStatusPageComponentDefinition(
+                "minecraft-server-motd",
+                "MinecraftServerStatusField"));
+        services.AddSingleton<IContentPageComponentDefinition>(
+            new MinecraftServerStatusPageComponentDefinition(
+                "minecraft-server-online-players",
+                "MinecraftServerStatusField"));
+        services.AddSingleton<IContentPageComponentDefinition>(
+            new MinecraftServerStatusPageComponentDefinition(
+                "minecraft-server-maximum-players",
+                "MinecraftServerStatusField"));
+        services.AddSingleton<IContentPageComponentDefinition>(
+            new MinecraftServerStatusPageComponentDefinition(
+                "minecraft-server-players",
+                "MinecraftServerStatusField"));
+        services.AddSingleton<IContentPageComponentDefinition>(
+            new MinecraftServerStatusPageComponentDefinition(
+                "minecraft-server-version",
+                "MinecraftServerStatusField"));
     }
 
-    private sealed class MinecraftServerStatusPageComponentDefinition : IContentPageComponentDefinition
+    private sealed class MinecraftServerStatusPageComponentDefinition(
+        string name,
+        string viewComponentName) : IContentPageComponentDefinition
     {
-        public string Name => "minecraft-server-status";
-        public string ViewComponentName => "MinecraftServerStatus";
+        public string Name { get; } = name;
+        public string ViewComponentName { get; } = viewComponentName;
 
         public void Validate(IReadOnlyDictionary<string, string> parameters)
         {
@@ -35,24 +69,53 @@ public sealed class MinecraftServerStatusPlugin : ISitePlugin
 
             var unsupported = parameters.Keys.Order(StringComparer.OrdinalIgnoreCase).First();
             throw new InvalidOperationException(
-                $"The minecraft-server-status page component does not support parameter '{unsupported}'.");
+                $"Content page component '{Name}' does not support parameter '{unsupported}'.");
         }
     }
 }
 
 public sealed class MinecraftServerStatusViewComponent : ViewComponent
 {
-    private readonly IMinecraftServerStatusService _statusService;
+    private readonly IMinecraftServerStatusSnapshotStore _store;
 
-    public MinecraftServerStatusViewComponent(IMinecraftServerStatusService statusService)
+    public MinecraftServerStatusViewComponent(IMinecraftServerStatusSnapshotStore store)
     {
-        _statusService = statusService;
+        _store = store;
     }
 
-    public async Task<IViewComponentResult> InvokeAsync(ContentPageComponentInvocation request)
+    public IViewComponentResult Invoke(ContentPageComponentInvocation request)
     {
         ArgumentNullException.ThrowIfNull(request);
-        var status = await _statusService.GetStatusAsync(HttpContext.RequestAborted);
-        return View("~/Views/Plugins/MinecraftServerStatus/Default.cshtml", status);
+        return View("~/Views/Plugins/MinecraftServerStatus/Default.cshtml", _store.Current);
+    }
+}
+
+public sealed class MinecraftServerStatusFieldViewComponent : ViewComponent
+{
+    private readonly IMinecraftServerStatusSnapshotStore _store;
+
+    public MinecraftServerStatusFieldViewComponent(IMinecraftServerStatusSnapshotStore store)
+    {
+        _store = store;
+    }
+
+    public IViewComponentResult Invoke(ContentPageComponentInvocation request)
+    {
+        ArgumentNullException.ThrowIfNull(request);
+        var field = request.Name.ToLowerInvariant() switch
+        {
+            "minecraft-server-status-badge" => "badge",
+            "minecraft-server-motd" => "motd",
+            "minecraft-server-online-players" => "online-players",
+            "minecraft-server-maximum-players" => "maximum-players",
+            "minecraft-server-players" => "players",
+            "minecraft-server-version" => "version",
+            _ => throw new InvalidOperationException(
+                $"Unsupported Minecraft status field component '{request.Name}'.")
+        };
+
+        return View(
+            "~/Views/Plugins/MinecraftServerStatus/Field.cshtml",
+            new MinecraftServerStatusFieldViewModel(field, _store.Current));
     }
 }
