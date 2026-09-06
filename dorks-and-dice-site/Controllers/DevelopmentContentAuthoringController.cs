098b292c7b03ee86232d2784d8649f6ce04c5c6f
Kyle Barnett <kbarnett0306@gmail.com>
2026-09-05 21:32:17 -0400

    Add trusted central content authoring route

---
+++ dorks-and-dice-site/Controllers/DevelopmentContentAuthoringController.cs
@@ -0,0 +1,24 @@
+using dorks_and_dice_site.Services.Content;
+using dorks_and_dice_site.Services.Content.Storage;
+using dorks_and_dice_site.Services.Identity;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dorks_and_dice_site.Controllers;
+
+[Authorize(Policy = AuthorizationPolicies.DevAccess)]
+[Route("development/content")]
+[RequestSizeLimit(ContentInputPolicy.MaxAuthoringRequestBytes)]
+public sealed class DevelopmentContentAuthoringController : ContentAuthoringControllerBase
+{
+    public DevelopmentContentAuthoringController(
+        IContentAuthoringService authoringService,
+        IContentBodyRenderer bodyRenderer,
+        IContentPageComposer pageComposer,
+        IContentSourceRegistry sourceRegistry)
+        : base(authoringService, bodyRenderer, pageComposer, sourceRegistry)
+    {
+    }
+
+    protected override bool IsCentralAuthoring => true;
+}
