0a4b1df3b11b69b3377edf3ad8f53b2a585b5c9d
Kyle Barnett <kbarnett0306@gmail.com>
2026-09-06 01:39:32 -0400

    Remove old shared Minecraft status contract

--- dorks-and-dice-site/Services/GameServers/Minecraft/IMinecraftServerStatusService.cs
+++
@@ -1,8 +0,0 @@
-using dorks_and_dice_site.Models.GameServers.Minecraft;
-
-namespace dorks_and_dice_site.Services.GameServers.Minecraft;
-
-public interface IMinecraftServerStatusService
-{
-    Task<MinecraftServerStatus> GetStatusAsync(CancellationToken cancellationToken = default);
-}
