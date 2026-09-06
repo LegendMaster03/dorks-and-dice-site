7df88b3c1c7755da66e15530124fe95da859ed3d
Kyle Barnett <kbarnett0306@gmail.com>
2026-09-05 21:38:48 -0400

    Bind editor landing page to active mode

--- dorks-and-dice-site/Controllers/EditorController.cs
+++ dorks-and-dice-site/Controllers/EditorController.cs
@@ -1,5 +1,4 @@
 using dorks_and_dice_site.Models.Editor;
-using dorks_and_dice_site.Models.Identity;
 using dorks_and_dice_site.Services.Identity;
 using dorks_and_dice_site.Services.Site;
 using Microsoft.AspNetCore.Authorization;
@@ -11,35 +10,16 @@ namespace dorks_and_dice_site.Controllers;
 [Route("editor")]
 public sealed class EditorController : Controller
 {
-    private readonly ISiteModeRegistry _siteModeRegistry;
-    private readonly SiteModeOptions _siteModeOptions;
-
-    public EditorController(
-        ISiteModeRegistry siteModeRegistry,
-        SiteModeOptions siteModeOptions)
-    {
-        _siteModeRegistry = siteModeRegistry;
-        _siteModeOptions = siteModeOptions;
-    }
-
     [HttpGet("")]
     public IActionResult Index()
     {
         var modeContext = HttpContext.GetSiteModeContext();
-        var hasGlobalEditor = AccountRoleHierarchy.PrincipalHasGlobalRole(User, AccountRoles.GlobalEditor);
-        var modes = _siteModeRegistry.All
-            .Where(mode => hasGlobalEditor || HasScopedEditorRole(mode.Id))
-            .Select(mode => new EditorModeOption
-            {
-                ModeId = mode.Id,
-                DisplayName = mode.DisplayName,
-                EditorHref = modeContext.HasTrustedAccess
-                    ? null
-                    : ResolvePublicEditorHref(modeContext.ActiveModeId, mode.Id)
-            })
-            .ToList();
-
-        if (modes.Count == 0)
+        var activeMode = modeContext.ActiveMode;
+        if (activeMode is null
+            || !AccountRoleHierarchy.PrincipalHasScopedRole(
+                User,
+                activeMode.Id,
+                ScopedAccountRoles.Editor))
         {
             return Forbid();
         }
@@ -47,25 +27,15 @@ public sealed class EditorController : Controller
         return View(new EditorIndexViewModel
         {
             IsTrustedPreview = modeContext.HasTrustedAccess,
-            Modes = modes
+            Modes =
+            [
+                new EditorModeOption
+                {
+                    ModeId = activeMode.Id,
+                    DisplayName = activeMode.DisplayName,
+                    EditorHref = "/editor/content"
+                }
+            ]
         });
-    }
-
-    private bool HasScopedEditorRole(string scope)
-    {
-        var expectedValue = $"{scope}:{ScopedAccountRoles.Editor}";
-        return User.HasClaim(AccountClaimTypes.ScopedRole, expectedValue);
-    }
-
-    private string? ResolvePublicEditorHref(string? activeModeId, string targetModeId)
-    {
-        if (string.Equals(activeModeId, targetModeId, StringComparison.OrdinalIgnoreCase))
-        {
-            return "/editor/content";
-        }
-
-        return _siteModeOptions.TryGetCanonicalHost(targetModeId, out var canonicalHost)
-            ? $"https://{canonicalHost}/editor/content"
-            : null;
     }
 }
