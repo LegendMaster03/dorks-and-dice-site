283b1126128c203fe6aa3eddc7ce01e904788634
Kyle Barnett <kbarnett0306@gmail.com>
2026-09-07 05:10:29 -0400

    Remove legacy Professional homepage and static resume assets

--- dorks-and-dice-site/Models/Resume/ResumeLineItemViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace dorks_and_dice_site.Models.Resume;
-
-public class ResumeLineItemViewModel
-{
-    public string Text { get; set; } = string.Empty;
-    public string CssClass { get; set; } = "mb-0";
-    public bool IsMuted { get; set; }
-}
--- dorks-and-dice-site/Services/Resume/IResumeContentService.cs
+++
@@ -1,8 +0,0 @@
-using dorks_and_dice_site.Models.Resume;
-
-namespace dorks_and_dice_site.Services.Resume;
-
-public interface IResumeContentService
-{
-    Task<ResumeViewModel> GetResumePageAsync(CancellationToken cancellationToken = default);
-}
