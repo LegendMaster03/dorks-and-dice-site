755e39a7efd3255eb5bc05ed5348d245e019b81f
Kyle Barnett <kbarnett0306@gmail.com>
2026-09-06 21:45:45 -0400

    Remove portfolio architecture model from generic site models

--- dorks-and-dice-site/Models/Site/SiteModeArchitectureSummaryViewModel.cs
+++
@@ -1,7 +0,0 @@
-namespace dorks_and_dice_site.Models.Site;
-
-public sealed class SiteModeArchitectureSummaryViewModel
-{
-    public IReadOnlyList<SiteModeSummaryRowViewModel> Modes { get; init; } = [];
-    public IReadOnlyList<RouteOwnershipProbeViewModel> RouteProbes { get; init; } = [];
-}
