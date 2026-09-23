3bf87a31b8c28c42f527b981b45b77ce7445b7ec
Kyle Barnett <kbarnett0306@gmail.com>
2026-09-23 17:21:25 -0400

    Fix OpenIddict registration marker declaration

--- dorks-and-dice-site/Services/Identity/AccountLinkingServiceCollectionExtensions.cs
+++ dorks-and-dice-site/Services/Identity/AccountLinkingServiceCollectionExtensions.cs
@@ -68,4 +68,6 @@ public static class AccountLinkingServiceCollectionExtensions
 }
 
 
-internal sealed class OpenIddictAccountLinkingRegistrationMarker;
+internal sealed class OpenIddictAccountLinkingRegistrationMarker
+{
+}
